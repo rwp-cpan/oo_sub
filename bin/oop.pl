b3b005fe9b7e852ad83f55e7b208ec52426db090
Elvin Aslanov <rwp.primary@gmail.com>
2022-12-12 14:52:32 +0400

    Add supporting infrastructure

---
+++ bin/oop.pl
@@ -0,0 +1,18 @@
+use v5.36;
+use lib '.';
+use oop;
+use DDP; # CPAN: Data-Printer
+
+say p my $user = getpwnam('root');
+
+say p my $group = getgrgid(0);
+
+say p my $file = stat('.');
+
+say p my $time = localtime('.');
+
+say p my $network = getnetbyname('loopback');
+
+say p my $protocol = getprotobyname('tcp');
+
+say p my $service = getservbyname('ftp');
--- lib/oop.pm
+++ lib/oop.pm
@@ -1,9 +1,9 @@
-package oop;
+package oop v0.1;
 
 use strict;
 use warnings;
 
-use Module::Load 'autoload_remote';
+use Module::Load;
 
 use User::pwent;
 use User::grent;
@@ -24,9 +24,67 @@ my @modules = qw(
 );
 
 for my $module ( @modules ) {
-	autoload_remote 'main', $module;
+	Module::Load::autoload_remote 'main', $module;
 }
 
 # https://perldoc.perl.org/Module::Load#autoload_remote
 
 1;
+
+=pod
+
+=head1 NAME
+
+oop - Use object-oriented versions of built-in Perl functions
+
+=head1 SYNOPSIS
+
+say p my $user = getpwnam('root');
+
+say p my $group = getgrgid(0);
+
+say p my $file = stat('.');
+
+say p my $time = localtime('.');
+
+say p my $network = getnetbyname('loopback');
+
+say p my $protocol = getprotobyname('tcp');
+
+say p my $service = getservbyname('ftp');
+
+=head1 DESCRIPTION
+
+Imports the following modules to enable OOP in Perl for some built-in functions:
+
+=over 2
+
+=item L<User::pwent>
+
+=item L<User::grent>
+
+=item L<File::stat>
+
+=item L<Time::Piece>
+
+=item L<Net::netent>
+
+=item L<Net::protoent>
+
+=item L<Net::servent>
+
+=back
+
+Uses L<autoload_remote|Module::Load/autoload_remote> to achieve this.
+
+=cut
+
+=head1 SEE ALSO
+
+L<User::pwent>, L<User::grent>, L<File::stat>, L<Time::Piece>, L<Module:Load>
+
+=head1 AUTHOR
+
+Elvin Aslanov
+
+=cut
